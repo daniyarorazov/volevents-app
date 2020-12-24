756fa472a93f94e04d8d3c9cefcba4184d2fefd5
Daniyar <orazov.video@gmail.com>
2020-12-25 01:51:58 +0600

    Commit 5
    1. Add new code to Add project.js
    2. Add new code to Add project.scss
    3. Edit some scss files

--- src/AddProject.js
+++ src/AddProject.js
@@ -1,9 +1,10 @@
 import React, { useState, useCallback } from 'react';
 import app from "./base.js";
 import { withRouter } from "react-router";
+import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from "uuid";
 import './DefaultStyles.css';
-import './AddProject.css';
+import './AddProject.scss';
 import Sidebar from './Sidebar';
 
 
@@ -62,82 +63,74 @@ const AddProject = ({history}) => {
             <Sidebar />
 
             <main>
-                <h2 className="main-title">Добавление мероприятия</h2>
-                <div className="main-block">
-                    <form onSubmit={handleAddProject} className="main-form">
-                        <div className="group">
-                            <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} required />
-                            <span className="highlight"></span>
-                            <span className="bar"></span>
-                            <label>Название проекта</label>
-                        </div>
-                        <div className="group">
-                            <input type="text" name="date" value={date} onChange={(e) => setDate(e.target.value)} required />
-                            <span className="highlight"></span>
-                            <span className="bar"></span>
-                            <label>Дата мероприятия</label>
-                        </div>
-                        <div className="group">
-                            <input type="text" name="time" value={time} onChange={(e) => setTime(e.target.value)} required />
-                            <span className="highlight"></span>
-                            <span className="bar"></span>
-                            <label>Время начала</label>
-                        </div>
-                        <div className="group">
-                            <input type="text" name="geolocation" value={geolocation} onChange={(e) => setGeolocation(e.target.value)} required />
-                            <span className="highlight"></span>
-                            <span className="bar"></span>
-                            <label>Место проведения</label>
-                        </div>
-                        <div className="group">
-                            <h2>Бесплатная еда</h2>
-                            <div className="group-radio">
-                                <div className="radio">
-                                    <input className="radio__input" type="radio" name="foodRadio" value={"Yes"} onChange={(e) => setFoodRadio(e.target.value)} id="radio1" />
-                                    <span className="radio__text">Да</span>
-                                </div>
-                                <div className="radio">
-                                    <input className="radio__input" type="radio" name="foodRadio" value={"No"} onChange={(e) => setFoodRadio(e.target.value)} id="radio2" />
-                                    <span className="radio__text">Нет</span>
+                <div className="section-add-project">
+                    <h2 className="section-title">Добавление мероприятия</h2>
+                    <div className="section-block">
+                        <form onSubmit={handleAddProject} className="main-form">
+                            <div className="group">
+                                <span class="group-label">Название</span>
+                                <input type="text" class="group-form" name="title" value={title} onChange={(e) => setTitle(e.target.value)} required />
+                            </div>
+                            <div className="group">
+                                <span class="group-label">Дата начала</span>
+                                <input type="text" class="group-form" name="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+                            </div>
+                            <div className="group">
+                                <span class="group-label">Время начала</span>
+                                <input type="text" class="group-form" name="time" value={time} onChange={(e) => setTime(e.target.value)} required />
+                            </div>
+                            <div className="group">
+                                <span class="group-label">Место проведения</span>
+                                <input type="text" class="group-form" name="geolocation" value={geolocation} onChange={(e) => setGeolocation(e.target.value)} required />
+                            </div>
+                            <div className="group">
+                                <span class="group__subtitle">Бесплатная еда</span>
+                                <div className="group-radio">
+                                    <div className="radio">
+                                        <input className="radio__input" type="radio" name="foodRadio" value={"Yes"} onChange={(e) => setFoodRadio(e.target.value)} id="radio1" />
+                                        <span className="radio__text">Да</span>
+                                    </div>
+                                    <div className="radio">
+                                        <input className="radio__input" type="radio" name="foodRadio" value={"No"} onChange={(e) => setFoodRadio(e.target.value)} id="radio2" />
+                                        <span className="radio__text">Нет</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="group">
-                            <h2>Бесплатная развозка</h2>
-                            <div className="group-radio">
-                                <div className="radio">
-                                    <input className="radio__input" type="radio" name="transportRadio" value={"Yes"} onChange={(e) => setTransportRadio(e.target.value)} id="radio1" />
-                                    <span className="radio__text">Да</span>
-                                </div>
-                                <div className="radio">
-                                    <input className="radio__input" type="radio" name="transportRadio" value={"No"} onChange={(e) => setTransportRadio(e.target.value)} id="radio2" />
-                                    <span className="radio__text">Нет</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="group">
-                            <h2>Бесплатное оборудование</h2>
-                            <div className="group-radio">
-                                <div className="radio">
-                                    <input className="radio__input" type="radio" name="equipmentRadio" value={"Yes"} onChange={(e) => setEquipmentRadio(e.target.value)} id="radio1" />
-                                    <span className="radio__text">Да</span>
-                                </div>
-                                <div className="radio">
-                                    <input className="radio__input" type="radio" name="equipmentRadio" value={"No"} onChange={(e) => setEquipmentRadio(e.target.value)} id="radio2" />
-                                    <span className="radio__text">Нет</span>
+                            <div className="group">
+                                <span class="group__subtitle">Бесплатная развозка</span>
+                                <div className="group-radio">
+                                    <div className="radio">
+                                        <input className="radio__input" type="radio" name="transportRadio" value={"Yes"} onChange={(e) => setTransportRadio(e.target.value)} id="radio1" />
+                                        <span className="radio__text">Да</span>
+                                    </div>
+                                    <div className="radio">
+                                        <input className="radio__input" type="radio" name="transportRadio" value={"No"} onChange={(e) => setTransportRadio(e.target.value)} id="radio2" />
+                                        <span className="radio__text">Нет</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="group">
-                            <textarea type="text" name="description" maxLength="1000" value={description} onChange={(e) => setDescription(e.target.value)} required></textarea>
-                            <span className="highlight"></span>
-                            <span className="bar"></span>
-                            <label>Описание мероприятия</label>
-                        </div>
-                        <button type="submit" className="button">Создать</button>
-                    </form>
+                            <div className="group">
+                                <span class="group__subtitle">Бесплатное оборудование</span>
+                                <div className="group-radio">
+                                    <div className="radio">
+                                        <input className="radio__input" type="radio" name="equipmentRadio" value={"Yes"} onChange={(e) => setEquipmentRadio(e.target.value)} id="radio1" />
+                                        <span className="radio__text">Да</span>
+                                    </div>
+                                    <div className="radio">
+                                        <input className="radio__input" type="radio" name="equipmentRadio" value={"No"} onChange={(e) => setEquipmentRadio(e.target.value)} id="radio2" />
+                                        <span className="radio__text">Нет</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="group">
+                                <span className="group-label">Описание мероприятия</span>
+                                <textarea type="text" className="group-form label-description" name="description" maxLength="1000" value={description} onChange={(e) => setDescription(e.target.value)} required></textarea>
+                            </div>
+                            <button type="submit" className="add-project__button-create">Создать</button>
+                        </form>
+                    </div>
+                    <Link to="/" className="add-project__button-cancel"><span>Отмена</span></Link>
                 </div>
-                <button className="button button-cancel">Отмена</button>
             </main>
         </>
     )
