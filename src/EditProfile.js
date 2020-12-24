7fb208c34a74d5d793fd5d57afd489df1355b2fa
Daniyar <orazov.video@gmail.com>
2020-12-24 15:23:49 +0600

    Commit 4.4
    1. Edit image in EditProfil.js
    2. Edit code in EditProfile.scss

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -44,13 +44,13 @@ const EditProfile = ({history}) => {
                 <div className="section-edit-profile">
                     <div className="header-edit-profile">
                         <div className="header-edit-profile-items">
-                            <img src={require("./images/EditUserEmail/edit-email-header-image.svg").default} alt="img-header" className="header-edit-profile-image"/>
+                            <img src={require("./images/EditProfile/edit-profile-icon.svg").default} alt="img-header" className="header-edit-profile-image"/>
                             <h4 className="header-title">Изменение основных данных</h4>
                         </div>
                         <hr />
                     </div>
                     <div className="section-body">
-                        <img className="edit-profile-image" src={require('./images/EditUserEmail/edit-email-image.svg').default} alt="profile image" className="edit-profile-image"/>
+                        <img className="edit-profile-image" src={require('./images/EditProfile/edit-profile-img.svg').default} alt="profile image" className="edit-profile-image"/>
                         <div className="block-edit-profile">
                             <form onSubmit={handleEditProfile} className="edit__form-block">
                                 <input 
